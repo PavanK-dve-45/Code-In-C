e54394a208f38491cb8e4cd57d1b14e1088420bd
PavanK dev 45 <pavankumarhk411@gmail.com>
2024-02-07 10:34:54 +0530

    Added 2 C files

---
+++ Fibonnacci_series.c
@@ -0,0 +1,21 @@
+//WAP to Display Fibonacci Series Using C Programming Language.
+#include<stdio.h>
+void main()
+{
+    int n,n1=0,n2=1,n3;
+    printf("Enter Number of Fibonacci Series u want: ");
+    scanf("%d",&n);
+    printf("%d  %d  ",n1,n2);
+    for (int i = 2; i < n; i++)
+    {
+        n3=n1+n2;
+        n1=n2;
+        n2=n3;
+        printf("%d  ",n3);
+    }
+}
+/*
+OUTPUT:
+Enter Number of Fibonacci Series u want: 10
+0  1  1  2  3  5  8  13  21  34
+*/
---
+++ String_Palindrom.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+#include<string.h>
+void main()
+{
+    char name[10];
+    printf("Enter Name: ");
+    scanf("%s",name);
+    strlwr(name);
+    int n=strlen(name);
+    char temp[n];
+    char temp1[n];
+    strcpy(temp,name);
+    printf("%s\n",temp);
+    strcpy(temp1,strrev(temp));
+    int i=strcmp(temp1,name);
+    if(i==0)
+    printf("It's a palindrom\n");
+    else
+    printf("It's not a palindrom\n");
+}
+/*
+OUTPUT:
+Enter Name: Nayan
+It's a palindrom
+
+Enter Name: Gadag
+gadag
+It's a palindrom
+*/
